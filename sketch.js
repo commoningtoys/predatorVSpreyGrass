7662b80e12db2a105fce493d2dfc81839ab8e0a2
Yyyyaaaannnnoooo <yannpatrickmartins@gmail.com>
2018-05-09 11:57:03 +0200

    FIX ERROR

--- sketch.js
+++ sketch.js
@@ -1,9 +1,6 @@
 const CELL = 80;
 let am = null, w, h, agentSize = 10, play = false, showMenu = false, resizeBox, speed = 50;
 let cnv;
-let osc;
-let oscPrey;
-let oscPredator;
 function setup() {
 	w = floor(minimum() / CELL) * CELL;
 	agentSize = w / CELL;
@@ -12,16 +9,6 @@ function setup() {
 	pixelDensity(1);
 	noStroke();
 	initModel();
-	///////////
-	// sound //
-	///////////
-	// osc = new p5.SinOsc(240);
-	// oscPredator = new p5.SinOsc(240);
-	// oscPrey = new p5.SinOsc(240);
-	// osc.amp(0);
-	// osc.start();
-	// oscPredator.start();
-	// oscPrey.start();
 }
 
 function draw() {
@@ -42,9 +29,7 @@ function draw() {
 		}
 		am.show();
 		am.infographic();
-		// let freqPred = map(predator[predator.length -1], 0, 100, 0, 440);
-		// let freqPrey = map(prey[prey.length -1], 0, 100, 0, 440);
-		// osc.freq(oscPredator);
+
 		// document.getElementById('frameRate').innerHTML = frameRate();
 }
 
@@ -68,8 +53,8 @@ function minimum(){
 
 function windowResized(){
 	w = floor(minimum() / CELL) * CELL;
-	// agentSize = w / CELL;
-	// am.setPixSize(agentSize);
+	agentSize = w / CELL;
+	am.setPixSize(agentSize);
 	resizeCanvas(w, innerHeight);
 }
 
@@ -78,7 +63,8 @@ function initModel() {
 	let maxHealth = document.getElementById("maxHealth").value;
 	let empty = document.getElementById("empty").value;
 	let prey = document.getElementById("prey").value;
-	am = new AgentModel(threshold, empty, prey, maxHealth);
+	console.log(threshold, empty, prey, maxHealth)
+	am = new AgentModel(agentSize, threshold, empty, prey, maxHealth);
 }
 function updateValue() {
 	document.getElementById("thehreshold").innerHTML = "THRESHOLD: " + document.getElementById("threshold").value;
